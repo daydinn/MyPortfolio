c32f4095689f21d8990984ca41bd6794b1f94364
daydinn <58174442+daydinn@users.noreply.github.com>
2024-08-29 15:22:56 +0200

    Small text change in About Section

--- src/components/Infos.jsx
+++ src/components/Infos.jsx
@@ -11,13 +11,13 @@ const Infos = () => {
         <h2 className="text-2xl font-bold mb-4">CV</h2>
         <div className="flex justify-center">
           <iframe
-            src="/assets/CV_Diyar_Aydin.pdf"
+            src="/assets/CV_Diyar_Aydin1.pdf"
             title="CV"
             className="w-full max-w-4xl h-96 border border-teal-500 rounded-lg shadow-lg"
           ></iframe>
         </div>
         <a
-          href="/assets/CV_Diyar_Aydin.pdf"
+          href="/assets/CV_Diyar_Aydin1.pdf"
           download
           className="mt-6 inline-flex items-center px-4 py-2 border border-teal-500 text-teal-500 rounded-lg transition duration-300 hover:bg-teal-500 hover:text-white"
         >

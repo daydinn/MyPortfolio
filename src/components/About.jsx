0ba03dc638d6c1df4e1bc4386b6cb662169d1099
daydinn <58174442+daydinn@users.noreply.github.com>
2024-09-12 02:42:02 +0200

    Neue Änderungen in CV

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <div className="flex flex-col items-center">
       <img src={profilePic} alt="Profile" className="w-52 h-52 rounded-full object-cover mb-6"/>
       <p className="max-w-2xl text-center">As a passionate programmer, I have extensive experience in developing outstanding websites and applications. My skills include frontend technologies such as HTML, CSS, SCSS, Bootstrap, Tailwind CSS, JavaScript, TypeScript, and Java, as well as frameworks and libraries like Angular and React. Additionally, I am proficient in design tools like Figma and Adobe XD.
-      Furthermore, I am able to quickly adapt to new environments and teams, and I highly value a good communication with colleagues.</p>
+       I am able to quickly adapt to new environments and teams, and I highly value a good communication with colleagues.</p>
       </div>
       <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none" style={{ height: '70px' }}>
         <svg className="relative block w-full h-full" viewBox="0 0 100 100" preserveAspectRatio="none">
